cf8d6abf55cd1238f64b80d2effa2b1d8125c212
Jason Spiller <jason@thetargetlab.com>
2018-04-16 05:23:22 -0600

    Updating to my file structure and adding 404.

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 /* dependencies & app setup */
 const express = require('express');
 const path = require('path');
-const logger = require('morgan');
+const morgan = require('morgan');
 const app = express();
 
-/* setting up port & listen */
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, function() {
-  console.log(`listening on port ${PORT}`);
-});
+// add our middleware!
+app.use(morgan('dev'));
+app.use(express.static('public'));
 
 /* set the view engine */
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-/* error logger, static routes */
-app.use(logger('dev'));
-app.use('/static', express.static(path.join(__dirname, 'public')));
+// /* error logger, static routes */
+// app.use(logger('dev'));
+// app.use('/static', express.static(path.join(__dirname, 'public')));
 
 
-app.get("/sample", function(req,res){
-	// here's a sample route
-})
 
-/* error handler */
-app.get('*', function(req, res) {
-  res.status(404).send({message: 'Oops! Not found.'});
+// last resort 404
+app.get('*', (req, res) => {
+	res.sendFile('/404.html', {root:'public'});
+});
+
+/* setting up port & listen */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log(`Hello Dave.`);
 });
