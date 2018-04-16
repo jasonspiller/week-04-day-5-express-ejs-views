e38de9839e83e6c3c8eaf5553a2d45956c9df465
Jason Spiller <jason@thetargetlab.com>
2018-04-16 14:15:07 -0600

    Fixed a bug.

--- routes/pizza.js
+++ routes/pizza.js
@@ -19,7 +19,7 @@ pizzasRoute.get('/:id', function(req, res) {
 pizzasRoute.get('/', function(req, res) {
 
 	let data = {
-		title: 'Pizzas',
+		strTitle: 'Pizzas',
 		arrPizzas: pizzas
 	}
 	res.render('./pizza/pizza-index', data);
